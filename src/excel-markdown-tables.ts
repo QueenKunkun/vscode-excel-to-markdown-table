2be621a4bc562c1d5939c4bef8af95a94b99d246
kk <742711819@qq.com>
2025-10-15 19:17:51 +0800

    fix: escape pipe with backslash

--- src/excel-markdown-tables.ts
+++ src/excel-markdown-tables.ts
@@ -10,7 +10,13 @@ export function excelToMarkdown(rawData: string): string {
     let data = rawData.trim();
     var intraCellNewlineReplacedData = helper.replaceIntraCellNewline(data)
     var rows = helper.splitIntoRowsAndColumns(intraCellNewlineReplacedData);
-    var {columnWidths, colAlignments } = helper.getColumnWidthsAndAlignments(rows);
+    rows = rows.map(function (row, index) {
+        return row.map(function (column, index) {
+            const res = column.replace(/\|/g, '\\|');
+            return res;
+        });
+    })
+    var { columnWidths, colAlignments } = helper.getColumnWidthsAndAlignments(rows);
     const markdownRows = helper.addMarkdownSyntax(rows, columnWidths);
 
     return helper.addAlignmentSyntax(markdownRows, columnWidths, colAlignments).join(LINE_ENDING);
